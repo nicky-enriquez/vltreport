bfc270723803458245f9408904084d9b95c405be
Nicky Enriquez <nicky.enriquez@kurax.dev>
2025-04-28 10:14:43 -0500

    add command processfilesftp

---
+++ backend/app/Console/Commands/processFileSFTP.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+
+class processFileSFTP extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'app:process-file-s-f-t-p';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        //
+    }
+}
